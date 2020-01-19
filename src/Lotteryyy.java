8aebde5c0efeb5844cae1d7269aa70ac656b168f
League Student <league@iMac-23.attlocal.net>
2020-01-19 14:30:16 -0800

    8-BAAL THJG

---
+++ src/Lotteryyy.java
@@ -0,0 +1,35 @@
+import java.util.Random;
+
+import javax.swing.JOptionPane;
+
+public class Lotteryyy {
+            public static void main(String[] args) {
+				
+              	
+    	
+      
+
+         JOptionPane.showMessageDialog(null,new Random().nextInt(5); + new Random().nextInt(5); +   new Random().nextInt(5); + new Random().nextInt(5); +   new Random().nextInt(5); 
+         
+            	
+            	
+            	
+            	
+            	
+            	
+            	
+            	
+            	
+            	
+            	
+            	
+            	
+            	
+            	
+            	
+            	
+            	
+            	
+            	
+			}
+}
--- src/_05_magic_8_ball/Magic8Ball.java
+++ src/_05_magic_8_ball/Magic8Ball.java
@@ -4,32 +4,51 @@
 
 package _05_magic_8_ball;
 
+import java.awt.Component;
 import java.util.Random;
 
+import javax.swing.JOptionPane;
+
 public class Magic8Ball {
+ public static void main(String[] args) {
+	
 
 	// 1. Make a main method that includes all the steps below….
 
 	// 2. Make a variable that will hold a random number and put a random number into this variable using "new Random().nextInt(4)"
-
+int ask = new Random().nextInt(4);
 	// 3. Print out this variable
-
+System.out.println(ask);
 	// 4. Get the user to enter a question for the 8 ball
-
+JOptionPane.showInputDialog(null, "ask a question please");
 	// 5. If the random number is 0
-
+if (ask == 0) { 
+JOptionPane.showMessageDialog(null, "yes!");
+}
 	// -- tell the user "Yes"
 
 	// 6. If the random number is 1
-
+if (ask==1) {
+JOptionPane.showMessageDialog(null, "N̷̟̈́ ̷̼̂͗Ȍ̸͔");
+}
 	// -- tell the user "No"
 
 	// 7. If the random number is 2
+if (ask ==2) {
+	
+	JOptionPane.showMessageDialog(null, "maybe you should ask google?");
+
+}
 
 	// -- tell the user "Maybe you should ask Google?"
 
 	// 8. If the random number is 3
-
+if (ask==3) {
+	JOptionPane.showMessageDialog(null, "outlook not good");
+}
 	// -- write your own answer
-
+if (ask==4) {
+	JOptionPane.showMessageDialog(null, "perhaps ( ͡° ͜ʖ ͡°)");
+}
+}
 }
